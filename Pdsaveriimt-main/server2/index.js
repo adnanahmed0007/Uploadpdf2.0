bedd2cca02a3ee2e8f0e89059b660e07aede81ce
adnanahmed0007 <139644036+adnanahmed0007@users.noreply.github.com>
2026-02-14 14:57:14 +0530

    added

--- Pdsaveriimt-main/server2/index.js
+++ Pdsaveriimt-main/server2/index.js
@@ -29,7 +29,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:5173",
-            "https://uploadpdf2-0.vercel.app",
+            "https://uploadpdf2-0.vercel.app/",
         ],
         credentials: true,
     })
