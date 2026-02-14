305bf8d604892cc7abe3a445bb9b62e5788ea4cc
adnanahmed0007 <139644036+adnanahmed0007@users.noreply.github.com>
2026-02-14 15:48:28 +0530

    added

--- Pdsaveriimt-main/server2/controllers/Authenticationusers/UserInfo.js
+++ Pdsaveriimt-main/server2/controllers/Authenticationusers/UserInfo.js
@@ -1,29 +1,26 @@
-const UserInfo=async(req,res,next)=>
-{
-    try{
-   const findUser=req.findId;
-   if(!findUser)
-   {
-    return res
-    .status(400)
-    .json({message:" not find the user"})
-   }
-   return res
-   .status(200)
-   .json({
-    message:"we got the user details",
-    findUser
+const UserInfo = async (req, res, next) => {
+    try {
+        const findUser = req.user;
+        if (!findUser) {
+            return res
+                .status(400)
+                .json({ message: " not find the user" })
+        }
+        return res
+            .status(200)
+            .json({
+                message: "we got the user details",
+                findUser
 
-   })
+            })
     }
-    catch(e)
-    {
+    catch (e) {
         console.log(e);
         return res
-        .status(400)
-        .json({
-            message:"server error"
-        })
+            .status(400)
+            .json({
+                message: "server error"
+            })
     }
 }
 export default UserInfo
